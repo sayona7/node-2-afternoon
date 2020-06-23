0982753b2da367518964466e6a5cf47826977b87
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-06-23 15:50:29 -0500

    set up tthe index.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 30001;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
