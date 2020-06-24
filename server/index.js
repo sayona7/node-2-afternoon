f89bfdae648ee476923da43d231040438adb23b3
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-06-23 21:11:32 -0500

    further progress in homework

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
+// reqs
 const express = require("express");
 const cntrl = require("./controllers")
 
+// app
 const app = express();
 
+// parsing
 app.use(express.json());
 
+// endpoints
+const messURL = "api/messages";
+app.post(messURL, cntrl.create);
+app.get(messURL, cntrl.read);
+app.put(`${messURL}/:id`, cntrl.update);
+app.delete(`${messURL}/:id`, cntrl.delete);
+
+// port
 const port = 3001;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
